d2dac1a83124bd7379978c6427b35ea9659f1bc2
Hafiz Usama <87196383+hmusama36@users.noreply.github.com>
2021-07-10 12:35:36 +0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{ 
+    'name': 'Odoo Practice',
+
+    'summary': 'Odoo All Fields @api and Functions in it. ', 
+
+    'description': 'I Think This Module Change Your Life.', 
+
+    'author': 'Rana Rizwan',
+
+    'website': "http://www.bcube.com",
+
+    'depends': ['base'], 
+
+    'application': True, 
+
+    'data': [
+    	'views/view.xml',
+    	'menu.xml',
+    ], 
+    'css': ['static/src/css/report.css'],
+}
