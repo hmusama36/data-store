41e76a9376b3f660b0e190b8f811f10f69728891
Usama <hmusamarajpoot@gmail.com>
2021-07-13 15:45:05 +0500

    update it

--- al_burhan/model.py
+++ al_burhan/model.py
@@ -11,6 +11,8 @@ class BurhanCourse(models.Model):
 	_description = 'Burhan Course'
 
 
+	_rec_name = 'name'
+
 	name = fields.Char(string="Name")
 	course_name = fields.Many2many("burhan.subjects",string="Subject Name")
 
@@ -18,6 +20,7 @@ class BurhanCourse(models.Model):
 class BurhanSubjects(models.Model): 
 	_name = 'burhan.subjects'
 	_description = 'Burhan Subjects'
+	_rec_name = 'name'
 
 	name = fields.Char(string="Name")
 
@@ -25,6 +28,8 @@ class BurhanSubjects(models.Model):
 class BurhanCampus(models.Model): 
 	_name = 'burhan.campus'
 	_description = 'Burhan Campus'
+
+	_rec_name = 'name'
 
 	name = fields.Char(string="Name")
 
@@ -43,13 +48,15 @@ class EcubeResPatrtner(models.Model):
 
 	name = fields.Char(string="Name")
 	father_name = fields.Char(string="Father Name")
-	date_of_birth = fields.Date(string="Date Of Birth")
 	cnic = fields.Char(string='CNIC')
 	mobile_number = fields.Char(string="Mobile Number")
 	address = fields.Char(string="Address")
 	city = fields.Many2one("ecube.city",string="City")
 	nationality = fields.Char(string="Nationality")
 	email = fields.Char(string="Email")
+	course = fields.Many2one("burhan.course",string="Apply Course")
+	campus = fields.Many2one("burhan.campus",string="Apply Campus")
+	date = fields.Date(string="Apply Date")
 	gender = fields.Selection([
 	('male','Male'),
 	('female','Female')])
@@ -59,12 +66,102 @@ class EcubeResPatrtner(models.Model):
 		webbrowser.open('alburhan.org',new=2)
 
 
-	# def host(self):
+class CourseOffer(models.Model): 
+	_name = 'course.offer'
+	_description = 'Course Offer'
 
-	# 	webbrowser.open('192.168.18.54:8069', new=0)
+	course_offer = fields.Char(string="Course Offer")
 
-	# def google(self):
-	# 	webbrowser.open('google.com', new=1)
+	start_date = fields.Date(string="Start Date")
+	end_date = fields.Date(string="End Date")
+	tree_id = fields.One2many('course.offer.tree','campus')
+
+class CourseOfferTree(models.Model): 
+	_name = 'course.offer.tree'
+
+	_description = 'Course Offer Tree'
+
+	campus = fields.Many2one('burhan.campus',string="Campus")
+	start_date = fields.Date(string="Start Date")
+	end_date = fields.Date(string="End Date")
+	teacher = fields.Char(string="Teacher")
+
+
+class ApplicationForm(models.Model): 
+	_name = 'application.form'
+	_description = 'Form'
+
+	# _rec_name = 'campus_id'
+
+
+	applicant_name = fields.Char(string="Applicant Name")
+	father_name = fields.Char(string="Father Name")
+	cnic = fields.Char(string='CNIC')
+	address = fields.Char(string="Address")
+	mobile_number = fields.Char(string="Mobile Number")
+	email = fields.Char(string="Email")
+	app_city_id= fields.Many2one("ecube.city",string="City")
+	nationality = fields.Char(string="Nationality")
+	gender = fields.Selection([
+	('male','Male'),
+	('female','Female')], string="Gender")
+
+	app_course_id = fields.Many2one("burhan.course",string="Apply Course")
+	app_campus_id = fields.Many2one("burhan.campus",string="Apply Campus")
+	date = fields.Date(string="Apply Date")
+	stage = fields.Selection([
+	('draft','Draft'),
+	('interview_call','Interview Call'),
+	('first_interview','First Interview'),
+	('second_interview','Second Interview'),
+	('enrollment','Enrollment')], default='draft')
+
+
+
+	def draft(self):
+		self.stage = 'draft'
+
+	def call(self):
+		self.stage = 'interview_call'
+	def first(self):
+		self.stage = 'first_interview'
+	def second(self):
+		self.stage = 'second_interview'
+	def enroll(self):
+		self.stage = 'enrollment'
+
+
+	
+		
+
+	@api.model
+	def create(self,val):
+		record = super(ApplicationForm, self).create(val)
+		record.create_student_form()
+		return record
+
+	def write(self,val):
+
+		record = super(ApplicationForm, self).write(val)
+		self.create_student_form()
+		return record
+
+	def create_student_form(self):
+		student_form = self.env['ecube.res.partner']
+		new_record_create_id = student_form.create({
+			'name' : self.applicant_name,
+			'father_name' : self.father_name,
+			'cnic' : self.cnic,
+			'mobile_number' : self.mobile_number,
+			'address' : self.address,
+			'email' : self.email,
+			'city' : self.app_city_id.id,
+			'nationality' : self.nationality,
+			'gender' : self.gender,
+			'course' : self.app_course_id.id,
+			'campus' : self.app_campus_id.id,
+			'date' : self.date,})
+
 
 
 	@api.onchange('email')
@@ -92,109 +189,6 @@ class EcubeResPatrtner(models.Model):
 				self.email=""
 				return {'value':{},'warning':{'title':
 					'warning','message':"minimum leght is 6"}}
-
-
-
-
-
-	@api.model
-	def create(self,val):
-		record = super(EcubeResPatrtner, self).create(val)
-		record.create_application_form()
-		return record
-
-	def write(self,val):
-
-		record = super(EcubeResPatrtner, self).write(val)
-		self.create_application_form()
-		return record
-
-	def create_application_form(self):
-		application_form = self.env['application.form']
-		new_record_create_id = application_form.create({
-			'applicant_name' : self.name,
-			'f_name' : self.father_name,
-			'cnic' : self.cnic,
-			'address' : self.address,
-			'mobile_number' : self.mobile_number,
-			'email' : self.email,
-			})
-
-class CourseOffer(models.Model): 
-	_name = 'course.offer'
-	_description = 'Course Offer'
-	# _rec_name = 'name'
-
-	course_offer = fields.Many2one("burhan.course",string="Course Offer")
-
-	start_date = fields.Date(string="Start Date")
-	end_date = fields.Date(string="End Date")
-	tree_id = fields.One2many('course.offer.tree','campus')
-
-class CourseOfferTree(models.Model): 
-	_name = 'course.offer.tree'
-
-	_description = 'Course Offer Tree'
-
-	# _rec_name = 'city'
-	campus = fields.Many2one('burhan.campus',string="Campus")
-	start_date = fields.Date(string="Start Date")
-	end_date = fields.Date(string="End Date")
-	teacher = fields.Char(string="Teacher")
-
-
-class ApplicationForm(models.Model): 
-	_name = 'application.form'
-	_description = 'Form'
-	# _rec_name = 'name'
-
-	applicant_name = fields.Char(string="Applicant Name")
-	f_name = fields.Char(string="Father Name")
-	cnic = fields.Char(string='Applicant CNIC')
-	address = fields.Char(string="Applicant Address")
-	mobile_number = fields.Char(string="Mobile Number")
-	email = fields.Char(string="Applicant Email")
-	gender = fields.Selection([
-	('male','Male'),
-	('female','Female')], string="Applicant Gender")
-
-	course = fields.Many2one("burhan.course",string="Apply Course")
-	campus = fields.Many2one("burhan.campus",string="Apply Campus")
-	date = fields.Date(string="Apply Date")
-	stage = fields.Selection([
-	('draft','Draft'),
-	('interview_call','Interview Call'),
-	('first_interview','First Interview'),
-	('second_interview','Second Interview'),
-	('enrollment','Enrollment')], default='draft')
-
-
-
-	def draft(self):
-		self.stage = 'draft'
-
-	def call(self):
-		self.stage = 'interview_call'
-	def first(self):
-		self.stage = 'first_interview'
-	def second(self):
-		self.stage = 'second_interview'
-	def enroll(self):
-		self.stage = 'enrollment'
-
-
-	
-		create = self.env['ecube.res.partner'].create({
-		'name': self.applicant_name,
-		'father_name': self.f_name,
-		'cnic': self.cnic,
-		'address': self.address,
-		'mobile_number': self.mobile_number,
-		'email': self.email,})
-
-
-
-
 
 
 
